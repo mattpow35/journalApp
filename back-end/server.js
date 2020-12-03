e23f96f727ea5d1f954c20de963f5bab5b8ef9be
Matt Powley <mattpow35@gmail.com>
2020-12-02 17:30:55 -0700

    try again

--- back-end/server.js
+++ back-end/server.js
@@ -48,8 +48,8 @@ app.post('/api/items', async (req, res) => {
   const item = new Item({
     name: req.body.title,
     message: req.body.path,
-    date: ,
-    path:
+    // date: ,
+    // path:
   });
   try {
     await item.save();
