8545a6dfc441356a809597b34614ae8f9ac9f3da
Matt Powley <mattpow35@gmail.com>
2020-12-02 17:27:48 -0700

    making changes to add to database

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 
+const multer = require('multer');
+const upload = multer({
+  dest: '/var/www/journalsharing.mattpowley.com/images/',
+  limits: {
+    fileSize: 10000000
+  }
+});
+
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -10,9 +19,48 @@ app.use(bodyParser.urlencoded({
 const mongoose = require('mongoose');
 
 // connect to the database
-mongoose.connect('mongodb://localhost:27017/museum', {
+mongoose.connect('mongodb://localhost:27017/journal', {
   useNewUrlParser: true
 });
+
+
+const itemSchema = new mongoose.Schema({
+  name: String,
+  message: String,
+  date: String,
+  path: String,
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+app.post('/api/photos', upload.single('photo'), async (req, res) => {
+  // Just a safety check
+  if (!req.file) {
+    return res.sendStatus(400);
+  }
+  res.send({
+    path: "/images/" + req.file.filename
+  });
+});
+
+app.post('/api/items', async (req, res) => {
+  var d = New Date();
+  const item = new Item({
+    name: req.body.title,
+    message: req.body.path,
+    date: d.toDateString(),
+    path: 
+  });
+  try {
+    await item.save();
+    res.send(item);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+
 
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
--- front-end/src/views/Create.vue
+++ front-end/src/views/Create.vue
@@ -1,67 +1,208 @@
 <template>
-<div>
-  <h1>Add New Journal Entry</h1>
-  <form v-if="creating" @submit.prevent="addEntry">
-    <input v-model="title" placeholder="Title">
-    <p></p>
-    <textarea v-model="problem" placeholder="Type your journal entry here..."></textarea>
-
-    <br />
-
-    <button type="submit">Add to Journal</button><br>
-
-
-  </form>
-  <div v-else>
-    <p>Journal entry added successfully!</p>
-    <p><a @click="toggleForm" href="#">Add another entry</a></p>
+<div class="admin">
+  <h1>The Admin Page!</h1>
+  <div class="heading">
+      <div class="circle">1</div>
+      <h2>Add an Item</h2>
+    </div>
+    <div class="add">
+      <div class="form">
+        <input v-model="title" placeholder="Title">
+        <textarea v-model="description" placeholder="Item description"></textarea>
+        <p></p>
+        <input type="file" name="photo" @change="fileChanged">
+        <button @click="upload">Upload</button>
+      </div>
+      <div class="upload" v-if="addItem">
+        <h2>{{addItem.title}}</h2>
+        <p> Descripton: {{addItem.description}}</p>
+        <img :src="addItem.path" />
+      </div>
+    </div>
+  <div class="heading">
+    <div class="circle">2</div>
+    <h2>Edit/Delete an Item</h2>
+  </div>
+  <div class="edit">
+    <div class="form">
+      <input v-model="findTitle" placeholder="Search">
+      <div class="suggestions" v-if="suggestions.length > 0">
+        <div class="suggestion" v-for="s in suggestions" :key="s.id" @click="selectItem(s)">{{s.title}}
+        </div>
+      </div>
+    </div>
+    <div class="upload" v-if="findItem">
+      <input v-model="findItem.title">
+      <textarea v-model="findItem.description"></textarea>
+      <p></p>
+      <img :src="findItem.path" />
+    </div>
+    <div class="actions" v-if="findItem">
+      <button @click="deleteItem(findItem)">Delete</button>
+      <button @click="editItem(findItem)">Edit</button>
+    </div>
   </div>
 </div>
+
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-  name: 'create',
+  name: 'Admin',
   data() {
     return {
-      creating: true,
-      title: '',
-      problem: '',
+      title: "",
+      description: "",
+      file: null,
+      addItem: null,
+      items: [],
+      findTitle: "",
+      findItem: null,
     }
   },
+  computed: {
+    suggestions() {
+      let items = this.items.filter(item => item.title.toLowerCase().startsWith(this.findTitle.toLowerCase()));
+      return items.sort((a, b) => a.title > b.title);
+    }
+  },
+  created() {
+    this.getItems();
+  },
   methods: {
-    toggleForm() {
-      this.creating = !this.creating;
+    async upload() {
+      try {
+        const formData = new FormData();
+        formData.append('photo', this.file, this.file.name)
+        let r1 = await axios.post('/api/photos', formData);
+        let r2 = await axios.post('/api/items', {
+          title: this.title,
+          path: r1.data.path,
+          description: this.description,
+        });
+        this.addItem = r2.data;
+      } catch (error) {
+        alert(error);
+      }
     },
-    addEntry() {
-      this.$root.$data.addEntry(this.title, this.problem, true);
-      this.title = "";
-      this.problem = "";
-      this.creating = false;
+    fileChanged(event) {
+      this.file = event.target.files[0]
     },
-
+    async getItems() {
+      try {
+        let response = await axios.get("/api/items");
+        this.items = response.data;
+        return true;
+      } catch (error) {
+        alert(error);
+      }
+    },
+    selectItem(item) {
+      this.findTitle = "";
+      this.findItem = item;
+    },
+    async deleteItem(item) {
+      try {
+        await axios.delete("/api/items/" + item._id);
+        this.findItem = null;
+        this.getItems();
+        return true;
+      } catch (error) {
+        alert(error);
+      }
+    },
+    async editItem(item) {
+      try {
+        await axios.put("/api/items/" + item._id, {
+          title: this.findItem.title,
+          description: this.findItem.description,
+        });
+        this.findItem = null;
+        this.getItems();
+        return true;
+      } catch (error) {
+        alert(error);
+      }
+    },
   }
 }
 </script>
 
+
+
 <style scoped>
-input {
-  font-size: 1.2em;
-  height: 30px;
-  width: 200px;
+.image h2 {
+  font-style: italic;
+  font-size: 1em;
 }
 
-textarea {
-  font-size: 1.6em;
-  width: 100%;
-  max-width: 500px;
-  height: 100px;
-}
-
-button {
+.heading {
+  display: flex;
+  margin-bottom: 20px;
   margin-top: 20px;
-  font-size: 1.2em;
 }
 
+p {
+font-size: 12;
+}
 
+.heading h2 {
+  margin-top: 8px;
+  margin-left: 10px;
+}
+
+.add,
+.edit {
+  display: flex;
+}
+
+.circle {
+  border-radius: 50%;
+  width: 18px;
+  height: 18px;
+  padding: 8px;
+  background: #333;
+  color: #fff;
+  text-align: center
+}
+
+/* Form */
+input,
+textarea,
+select,
+button {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1em;
+  display: flex;
+
+}
+
+.form {
+  margin-right: 50px;
+}
+
+/* Uploaded images */
+.upload h2 {
+  margin: 0px;
+}
+
+.upload img {
+  max-width: 300px;
+}
+
+/* Suggestions */
+.suggestions {
+  width: 200px;
+  border: 1px solid #ccc;
+}
+
+.suggestion {
+  min-height: 20px;
+}
+
+.suggestion:hover {
+  background-color: #5BDEFF;
+  color: #fff;
+}
 </style>
